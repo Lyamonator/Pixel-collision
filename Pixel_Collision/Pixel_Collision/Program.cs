a5e2495a9c6e22e155f54bdd66dd79326cc0093c
Lyam Katz <46765902+Lyamonator@users.noreply.github.com>
2020-12-13 14:20:36 -0500

    Create Program.cs

---
+++ Pixel_Collision/Pixel_Collision/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Pixel_Collision
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
